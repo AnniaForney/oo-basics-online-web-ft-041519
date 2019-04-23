eb8d41a0482d18a17428e1d68a69185c534c7b53
Annia Forney <anniaforney@gmail.com>
2019-04-23 21:46:11 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -5,14 +5,9 @@ class Shoe
     @brand = brand 
   end 
   
-  def new_shoe 
-    puts "Shoe is in new condition"
-  end 
-
-  def shoe_repair 
-    puts "Shoe has been repaired"
-  end 
-  
+ def condition 
+ end 
+ 
   def cobble 
     puts "Your shoe is as good as new!"
   end 
