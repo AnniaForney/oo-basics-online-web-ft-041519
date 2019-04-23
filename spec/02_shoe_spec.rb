4a02b448f9aab005c968f89ca7b88566d70a82f0
Annia Forney <anniaforney@gmail.com>
2019-04-23 21:38:08 +0000

    Automatically backed up by Learn

--- spec/02_shoe_spec.rb
+++ spec/02_shoe_spec.rb
@@ -1,8 +1,10 @@
+require pry 
 describe 'Shoe' do
   let(:shoe) { Shoe.new("Nike") }
 
   describe '::new' do
     it 'gets initialized with a brand' do
+      binding.pry 
       expect{Shoe.new("Adidas")}.to_not raise_error
     end
   end
